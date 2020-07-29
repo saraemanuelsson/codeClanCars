5a1526f2a1e1eea129e5811e82bd8ab53a642fed
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-29 14:43:15 +0100

    Add ElectricMotor class with charge battery method

--- src/main/java/ElectricMotor.java
+++ src/main/java/ElectricMotor.java
@@ -1,2 +1,20 @@
 public class ElectricMotor {
+
+    private int battery;
+
+    public ElectricMotor() {
+        this.battery = 100;
+    }
+
+    public int getBattery() {
+        return battery;
+    }
+
+    public void chargeBattery(int charge) {
+        if (this.battery + charge > 100) {
+            this.battery = 100;
+        }
+        this.battery += charge;
+    }
+
 }
