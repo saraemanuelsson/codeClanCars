354d3c6ab750b2cd1824aab6826aea452ddbc5a6
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-29 14:08:49 +0100

    Create vehicle abstract class with getters and setters for properties

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,2 +1,27 @@
-public class Vehicle {
+public abstract class Vehicle {
+
+    protected int price;
+    protected String colour;
+
+    public Vehicle(int price, String colour){
+        this.price = price;
+        this.colour = colour;
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+    public String getColour(){
+        return this.colour;
+    }
+
+    public void setPrice(int newPrice) {
+        this.price = newPrice;
+    }
+
+    public void setColour(String colour) {
+        this.colour =
+    }
+
 }
