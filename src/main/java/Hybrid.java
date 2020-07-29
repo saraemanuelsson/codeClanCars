ea7a64e5ded71fb3e59e47e65fb0ea5ad2495652
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-29 14:59:58 +0100

    Add hybrid class

--- src/main/java/Hybrid.java
+++ src/main/java/Hybrid.java
@@ -1,2 +1,28 @@
-public class Hybrid {
+public class Hybrid extends Vehicle{
+
+    private Engine engine;
+    private ElectricMotor electricMotor;
+
+    public Hybrid(int price, String colour, Engine engine, ElectricMotor electricMotor) {
+        super(price, colour);
+        this.engine = engine;
+        this.electricMotor = electricMotor;
+    }
+
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public ElectricMotor getElectricMotor() {
+        return electricMotor;
+    }
+
+    public void setElectricMotor(ElectricMotor electricMotor) {
+        this.electricMotor = electricMotor;
+    }
 }
--- src/test/java/HybridTest.java
+++ src/test/java/HybridTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class HybridTest {
+
+    Hybrid hybrid;
+    Engine engine;
+    ElectricMotor electricMotor;
+
+    @Before
+    public void before(){
+        engine = new Engine(60, 1300);
+        electricMotor = new ElectricMotor();
+        hybrid = new Hybrid(14000, "yellow", engine, electricMotor);
+    }
+
+    @Test
+    public void hasPrice(){
+        assertEquals(14000, hybrid.getPrice());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("yellow", hybrid.getColour());
+    }
+
+    @Test
+    public void hasEngine(){
+        assertEquals(60, hybrid.getEngine().getFullTank());
+        assertEquals(1300, hybrid.getEngine().getSize());
+    }
+
+    @Test
+    public void hasElectricMotor(){
+        assertEquals(100, hybrid.getElectricMotor().getBattery());
+    }
+
 }
